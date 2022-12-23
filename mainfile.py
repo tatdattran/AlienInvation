e8361e45f1b326ba8693eb772d84ce2aa4a578a4
tatdattran <44671178+tatdattran@users.noreply.github.com>
2022-12-23 13:59:26 +0100

    Add files via upload

---
+++ mainfile.py
@@ -0,0 +1,341 @@
+import pygame
+import pygame.font
+import sys
+from pygame.sprite import Group
+from pygame.sprite import Sprite
+from time import sleep
+import game_functions as gf
+
+# alien.py
+class Alien(Sprite):
+    """A class to represent a single alien in the fleet."""
+
+    def __init__(self, ai_settings, screen):
+        """Initialize the alien, and set its starting position."""
+        super(Alien, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Load the alien image, and set its rect attribute.
+        self.image = pygame.image.load("images/alien.bmp")
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact position.
+        self.x = float(self.rect.x)
+
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Move the alien right or left."""
+        self.x += self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Draw the alien at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+# bullet.py
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship."""
+
+    def __init__(self, ai_settings, screen, ship):
+        """Create a bullet object, at the ship's current position."""
+        super(Bullet, self).__init__()
+        self.screen = screen
+
+        # Create bullet rect at (0, 0), then set correct position.
+        self.rect = pygame.Rect(
+            0, 0, ai_settings.bullet_width, ai_settings.bullet_height
+        )
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        # Store a decimal value for the bullet's position.
+        self.y = float(self.rect.y)
+
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.y -= self.speed_factor
+        # Update the rect position.
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
+
+# button.py
+class Button:
+    def __init__(self, ai_settings, screen, msg):
+        """Initialize button attributes."""
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+
+        # Set the dimensions and properties of the button.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Build the button's rect object, and center it.
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        # The button message only needs to be prepped once.
+        self.prep_msg(msg)
+
+    def prep_msg(self, msg):
+        """Turn msg into a rendered image, and center text on the button."""
+        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        # Draw blank button, then draw message.
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
+
+# game_stats.py
+class GameStats:
+    """Track statistics for Alien Invasion."""
+
+    def __init__(self, ai_settings):
+        """Initialize statistics."""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+
+        # Start game in an inactive state.
+        self.game_active = False
+
+        # High score should never be reset.
+        self.high_score = 0
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
+
+# scoreboard.py
+class Scoreboard:
+    """A class to report scoring information."""
+
+    def __init__(self, ai_settings, screen, stats):
+        """Initialize scorekeeping attributes."""
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.ai_settings = ai_settings
+        self.stats = stats
+
+        # Font settings for scoring information.
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Prepare the initial score images.
+        self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
+        self.prep_ships()
+
+    def prep_score(self):
+        """Turn the score into a rendered image."""
+        rounded_score = int(round(self.stats.score, -1))
+        score_str = "{:,}".format(rounded_score)
+        self.score_image = self.font.render(
+            score_str, True, self.text_color, self.ai_settings.bg_color
+        )
+
+        # Display the score at the top right of the screen.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def prep_high_score(self):
+        """Turn the high score into a rendered image."""
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.ai_settings.bg_color
+        )
+
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def prep_level(self):
+        """Turn the level into a rendered image."""
+        self.level_image = self.font.render(
+            str(self.stats.level), True, self.text_color, self.ai_settings.bg_color
+        )
+
+        # Position the level below the score.
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        """Show how many ships are left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_settings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
+
+    def show_score(self):
+        """Draw score to the screen."""
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        # Draw ships.
+        self.ships.draw(self.screen)
+
+# settings.py
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's static settings."""
+        # Screen settings.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings.
+        self.ship_limit = 3
+
+        # Bullet settings.
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+        # Alien settings.
+        self.fleet_drop_speed = 10
+
+        # How quickly the game speeds up.
+        self.speedup_scale = 1.1
+        # How quickly the alien point values increase.
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 0.3
+
+        # Scoring.
+        self.alien_points = 50
+
+        # fleet_direction of 1 represents right, -1 represents left.
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings and alien point values."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+
+# ship.py
+class Ship(Sprite):
+    def __init__(self, ai_settings, screen):
+        """Initialize the ship, and set its starting position."""
+        super(Ship, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Load the ship image, and get its rect.
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
+        # Movement flags.
+        self.moving_right = False
+        self.moving_left = False
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.center = self.screen_rect.centerx
+
+    def update(self):
+        """Update the ship's position, based on movement flags."""
+        # Update the ship's center value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+def run_game():
+    # Initialize pygame, settings, and screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make the Play button.
+    play_button = Button(ai_settings, screen, "Play")
+
+    # Create an instance to store game statistics, and a scoreboard.
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
+    # Set the background color.
+    bg_color = (230, 230, 230)
+
+    # Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(
+            ai_settings, screen, stats, sb, play_button, ship, aliens, bullets
+        )
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+
+        gf.update_screen(
+            ai_settings, screen, stats, sb, ship, aliens, bullets, play_button
+        )
+run_game()
